4f2afb5a4b05bd636727891111bebf28b3fe7f65
Elias Lorandi <elias.lorandi@hotmail.com>
2023-05-30 14:05:45 -0300

    Realização de testes

--- app/models/Operation.php
+++ app/models/Operation.php
@@ -3,20 +3,36 @@
 namespace app\models;
 
 class Operation {
-    public function addition($a, $b){
-        return $a + $b;
+
+    public function addition($a = 0, $b = 0){
+        $total = $a + $b;
+        return $total;
+       
     }
 
     public function subtraction($a, $b){
-        return $a - $b;
-    }
+        if($a == null & $b == null){
+            $total = 0;
+            return $total;
+        } elseif($a = null){
+            return $b;
+        }elseif($b = null){
+            $total = $a;
+            return $total;
+        } 
+            $total = $a - $b;
+            return $total;
+        }
 
     public function multiplication($a, $b){
+        if($a==0 & $b==0){
+            return 0;
+        }
         return $a * $b;
     }
 
     public function division($a, $b){
-        if($b==0){
+        if($a==0 & $b==0){
             return 0;
         }
         return $a / $b;
